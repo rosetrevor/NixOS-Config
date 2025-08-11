084e69de3e98481ae29cf1bd48f13687d1d9d77a
Trevor <trevor.ros3@gmail.com>
2025-08-10 18:38:19 -0700

    Updated waybar config

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    proxmox-nixos.url = "github:SaumonNet/proxmox-nixos";
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}:
+  outputs = { self, nixpkgs, home-manager, proxmox-nixos, ...}:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
@@ -16,7 +17,21 @@
     nixosConfigurations = {
       trevbawt = lib.nixosSystem {
         inherit system;
-        modules = [ ./configuration.nix ];
+        modules = [ 
+	  ./configuration.nix
+	  proxmox-nixos.nixosModules.proxmox-ve
+
+	  ({ pkgs, lib, ... }: {
+	    services.proxmox-ve = {
+	      enable = true;
+	      ipAddress = "192.168.0.90";
+	    };
+
+	  nixpkgs.overlays = [
+	    proxmox-nixos.overlays.${system}
+	  ];
+	  })
+	];
       };
     };
     homeConfigurations = {
