924a382ee293c6d77478beb40bbabdef8c59ed41
Trevor <trevor.ros3@gmail.com>
2026-01-18 11:58:42 -0800

    Adding godot

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "JetBrains Mono" ]; })
     pkgs.nerd-fonts.jetbrains-mono
+    pkgs.godot
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
