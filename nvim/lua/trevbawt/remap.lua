e3fddcae9cdea463d8164825426d270dbd82cc75
Trevor <trevor.ros3@gmail.com>
2026-01-24 15:46:46 -0800

    Updated telescope keybindings

--- nvim/lua/trevbawt/remap.lua
+++ nvim/lua/trevbawt/remap.lua
@@ -8,3 +8,8 @@ vim.keymap.set('n', '<c-j>', '<cmd>TmuxNavigateDown<cr>')
 vim.keymap.set('n', '<c-k>', '<cmd>TmuxNavigateUp<cr>')
 vim.keymap.set('n', '<c-l>', '<cmd>TmuxNavigateRight<cr>')
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>grep', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>buf', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
